877e812203a7e8c9e3cc42154bc14fee2b3521c4
praj1001 <134480625+praj1001@users.noreply.github.com>
2023-11-07 14:53:28 +0530

    terraform scripts for gcp

--- terraform/gcp/modules/instances/variables.tf
+++ terraform/gcp/modules/instances/variables.tf
@@ -6,65 +6,54 @@ variable "project" {
 variable "name" {
   description = "Name of provisioned resource"
   type        = string
-  default     = "puppetlabs-provision"
 }
 
 variable "machine_type" {
   description = "Machine type to use for the instance"
   type        = string
-  default     = "e2-micro"
 }
 
 variable "region" {
   description = "GCP region that'll be targeted for infrastructure deployment"
   type        = string
-  default     = "us-west1"
+}
+
+variable "zones" {
+  description = "GCP zone that are within the defined GCP region that you wish to use"
+  type        = list(string)
 }
 
 variable "server_count" {
   description = "Number of servers to deploy"
   type        = number
-  default     = 1
 }
 
 variable "image" {
   description = "GCP Image to be used for the instance"
   type        = string
-  default     = "debian-cloud/debian-11"
 }
 
 variable "boot_disk_params" {
   description = "Boot disk device configuration"
   type        = map(any)
-  default = {
-    size = 10
-    type = "pd-ssd"
-  }
-  validation {
-    condition     = var.boot_disk_params.size >= 10
-    error_message = "Minium disk size for any OS is 10 GB."
-  }
 }
 
-variable "network" {
+variable network    {
   description = "VPC network provisioned by the networking submodule"
   type        = string
-  default     = "default"
 }
 
-variable "subnetwork" {
+variable subnetwork {
   description = "Regional subnetwork assigned to VPC network provisioned by the networking submodule"
   type        = string
-  default     = "default"
 }
 
-variable "subnetwork_project" {
+variable subnetwork_project {
   description = "Regional subnetwork project assigned to VPC network provisioned by the networking submodule"
   type        = string
 }
 
 variable "labels" {
   description = "A list of labels that will be applied to virtual instances"
-  type        = map(any)
-  default     = {}
+  type        = map
 }
